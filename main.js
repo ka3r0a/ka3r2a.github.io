7f7e6a939726d6383e809ccaa15499ce7e197aaa
ka3r0a <ax.t978@gmail.com>
2025-02-12 13:17:13 +0330

    Update main.js

--- main.js
+++ main.js
@@ -64,4 +64,11 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     })
   })
+
+  // Initialize progress bars
+  const progressBars = document.querySelectorAll('.progress-bar');
+  progressBars.forEach((bar) => {
+    const value = bar.getAttribute('aria-valuenow');
+    bar.style.width = `${value}%`;
+  });
 })
